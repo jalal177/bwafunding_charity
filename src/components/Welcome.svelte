9c6b7af0c407f71b259e908e067432bfe350280e
jalal177 <jalaljpr25@gmail.com>
2022-03-11 13:00:31 +0700

    SPA Feature

--- src/components/Welcome.svelte
+++ src/components/Welcome.svelte
@@ -1,25 +1,20 @@
 <section class="xs-welcome-slider">
-    
-
-    <div class="xs-banner-slider owl-carousel owl-loaded owl-drag">
-        <div class="owl-nav disabled">
-            <div class="owl-prev"></div>
-            <i class="fa fa-angle-left xs-round-nav"></i>
+  <div class="xs-banner-slider owl-carousel owl-loaded owl-drag">
+    <div class="owl-nav disabled">
+      <div class="owl-prev" />
+      <i class="fa fa-angle-left xs-round-nav" />
+    </div>
+    <div class="xs-welcome-content" style="background-image: url(assets/images/slide1.png);">
+      <div class="container">
+        <div class="xs-welcome-wraper color-white">
+          <h2>Hunger is stalking the globe</h2>
+          <p>Hundreds of thousands of children experiencing or witnessing assault <br /> and other gender-based violence.</p>
+          <a href="#popularcause" class="btn btn-outline-primary"> View Causes </a>
         </div>
-        <div class="xs-welcome-content" style="background-image: url(assets/images/slide1.png);">
-            <div class="container">
-                <div class="xs-welcome-wraper color-white">
-                    <h2>Hunger is stalking the globe</h2>
-                    <p>Hundreds of thousands of children experiencing or witnessing assault <br> and other
-                        gender-based violence.</p>
-                    <a href="#popularcause" class="btn btn-outline-primary">
-                        View Causes
-                    </a>
-                </div> 
-            </div> 
-            <div class="xs-black-overlay"></div>
-        </div> 
-        <!-- <div class="xs-welcome-content" style="background-image: url(assets/images/slide2.png);">
+      </div>
+      <div class="xs-black-overlay" />
+    </div>
+    <!-- <div class="xs-welcome-content" style="background-image: url(assets/images/slide2.png);">
             <div class="container">
                 <div class="xs-welcome-wraper color-white">
                     <h2>Let's free the nature at all</h2>
@@ -45,7 +40,5 @@
             </div> 
             <div class="xs-black-overlay"></div>
         </div>  -->
-    </div>
-   
-    
+  </div>
 </section>
---
+++ src/pages/NotFound.svelte
@@ -0,0 +1,27 @@
+<!-- bagian notfund -->
+<section>
+  <div class="notfund d-flex align-items-center text-center">
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="col-lg-6">
+          <img class="rounded" src="/assets/images/bg.jpg" />
+          <h2 class="notfund_Title font-weight-bold mt-2">Oops!</h2>
+          <p class="notfund_text">There is something wrong tge oage. I cannot find it</p>
+          <a href="/" class="btn btn-primary">BACK TO THE HOME PAGE</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- bagian css -->
+<style type="text/css">
+  .notfund {
+    width: 100%;
+    height: 100vh;
+  }
+  .notfund_Title {
+    font-size: 40px;
+    color: rgb(0, 80, 160);
+  }
+</style>
